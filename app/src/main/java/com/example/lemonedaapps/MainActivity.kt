5aae2b9bf7f703e4cf27c9e24a9e1b3a9ede30d8
Aghatareis <ma0929959@gmail.com>
2023-05-02 17:26:02 -0300

    Initial commit

---
+++ app/src/main/java/com/example/lemonedaapps/MainActivity.kt
@@ -0,0 +1,78 @@
+package com.example.lemonedaapps
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.lemonedaapps.ui.theme.LemonedaAppsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LemonedaAppsTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = Color.LightGray
+                ) {
+
+                    AppLimoneda()
+
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun AppLimoneda(){
+    Column (
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+        Text(
+            text = stringResource(R.string.limoeiro),
+            color = Color.Black,
+            modifier = Modifier
+                .padding(5.dp)
+                .padding(20.dp),
+            fontSize = 15.sp,
+            fontFamily = FontFamily.SansSerif
+        )
+        
+        Image(
+            painter = painterResource(id = R.drawable.lemon_tree),
+            contentDescription = null,
+            modifier = Modifier
+                .size(250.dp)
+                .border(
+                    BorderStroke(2.dp, Color.Cyan),
+                    RoundedCornerShape(5.dp)
+                )
+
+        )
+        
+    }
+}
